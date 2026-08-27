337bf38c6fdb7cdb27b882c16b44ecef1aa3833a
PomerTerren <ljmm9658@gmail.com>
2026-08-27 14:23:09 -0600

    actualizacion archivos

--- database.php
+++ database.php
@@ -1,16 +1,25 @@
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "business_db";
-    $conn = "";
 
+$db_server = "mysql";
+$db_user = "root";
+$db_pass = getenv("MYSQL_ROOT_PASSWORD");
+$db_name = "business_db";
 
+$conn = "";
 
-    try{
-        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-        echo "You are connected";
-    } catch(mysqli_sql_exception){
-        echo "Connection lost";
-    }
+try {
+
+    $conn = mysqli_connect(
+        $db_server,
+        $db_user,
+        $db_pass,
+        $db_name
+    );
+
+    echo "You are connected";
+
+} catch (mysqli_sql_exception) {
+
+    echo "Connection lost";
+}
 ?>
